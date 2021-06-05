0c5d5991a6b71632dd605e593eb7baf4fd9a056f
Carl Fredrick <carlfredrickthegreat@gmail.com>
2021-06-04 21:58:46 -0700

    added start button, linked buttons to js functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
+// Perform initial functionality when site loads
 window.onload = init();
 
-function init() {   // entry point for program. TODO: is there a better way to do this?
+// Global variable(s) (TODO: is this bad practice?)
+var p1_score;
+var p2_score;
+
+var game_in_progress;
+
+/* Perform initial functionality, which includes:
+    - Adding cards to both players' decks
+    - Shuffling both decks
+    - 
+*/
+function init() {
+    // Disable game window
+    document.getElementById("game").style.display = "none";
+    document.getElementById("game").style.display = "none";
+
+    // Set global variables
+    p1_score = 0;
+    p2_score = 0;
+    game_in_progress = true;
+
     let p1_cards = [];
     let p2_cards = [];
     for (let i = 0; i < 2; i++) {
@@ -16,6 +37,21 @@ function init() {   // entry point for program. TODO: is there a better way to d
     shuffle(p1_cards);
     shuffle(p2_cards);
     // console.log('shuffled cards', p1_cards);
+}
+
+/* When the player first starts the game:
+    - The game window is unhidden
+    - The start button is hidden (TODO: change)
+    -
+*/
+function start_game() {
+    document.getElementById("game").style.display = "block";
+    document.getElementById("game").style.display = "block";
+    document.getElementById("start").style.display = "none";
+}
+
+function player_draw_card(cards) {
+    return cards.splice(0, 1);
 }
 
 // Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
